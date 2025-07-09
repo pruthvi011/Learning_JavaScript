783a37ee705127057a98572e91b3b341ab6d0e16
pruthvi011 <morepruthviraj05@gmail.com>
2025-07-09 13:37:36 +0530

    started Learning JS

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Hello!");
+function Message(){
+            alert('External JS Eg.');
+}
+
+document.write("<br>");
+
+let word = "More Pruthviraj"
+
+document.write(word);
+document.write("<br>");
+document.write(word);
+document.write("<br>");
+document.write(word);
+document.write("<br>");
