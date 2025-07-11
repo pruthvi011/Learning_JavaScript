3d8f2c5732deafb1b5df9f805e62064c1b291cdf
pruthvi011 <morepruthviraj05@gmail.com>
2025-07-11 13:31:39 +0530

    learned some more topics

--- index.js
+++ index.js
@@ -12,3 +12,58 @@ document.write(word);
 document.write("<br>");
 document.write(word);
 document.write("<br>");
+
+
+// We can reassign & redeclare using Var 
+     var name = "More";
+     var name = "Pruthvi"
+
+// We can reassign & can't redeclare using Let 
+    let y=56;
+    y=85;
+
+// We can't reassign or redeclare using const
+
+    const roll_no = 21;
+
+
+    let a ="More";
+    let b =21;
+    let c =["HTML","CSS","JavaScript"];
+    let d ={StName:"Mahesh",StNo:54}
+    let e =null;
+
+    typeof a;
+    typeof b;
+    typeof c;
+    typeof d;
+    typeof e;
+
+
+    let p=17;
+    let q=3;
+    let r=q+r;
+
+    c++;
+    c--;
+    c++;
+    c--;
+    document.write(c);
+
+    var m = "15";
+    var n = 15;
+
+    // if (m == n) {
+    //     document.write(true);
+    // } else {
+    //     document.write(false);
+    // }
+
+    if (m === n) {
+        document.write(true);
+    } else {
+        document.write(false);
+    }
+
+
+
